7eeb978933ab213c991d0c63306559a090ecdfbc
SP-Upreti <surendraupreti441@gmail.com>
2024-12-26 18:26:01 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
-import Blogs from './components/navbar/posts/blogs'
+import Blogs from './components/posts/blogs'
 import Footer from './components/footer/footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />
